c5547b76c0747cf6392b14c2bd61b877ff55fe7f
hustcc <i@hust.cc>
2016-01-14 20:15:55 +0800

    modify cache use wrapcache

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,13 @@ Created on 2015年6月16日
 @author: hzwangzhiwei
 '''
 from flask import Flask
+import wrapcache
+from wrapcache.adapter.MemoryAdapter import MemoryAdapter
+from wrapcache.database import LruCacheDB
 
 app = Flask(__name__)
 app.secret_key = 'your_session_key_redis_monitor'
+
+MemoryAdapter.db = LruCacheDB(size = 20) #init the store db
 
 from app.views import main_views
--- app/monitors/RedisMonitor.py
+++ app/monitors/RedisMonitor.py
@@ -8,6 +8,27 @@ from app.wraps.singleton_wrap import singleton
 import hashlib
 import time
 import redis
+import wrapcache
+
+@wrapcache.wrapcache(timeout = 1)
+def new_request(host, port, password, charset = 'utf8'):
+    redis_rst = {}
+    #需要重新请求获得数据
+    try:
+        start = time.time()
+        
+        r = redis.Redis(host = host, port = int(port), password = password)
+        info = r.info()
+        end = time.time();
+        info['get_time'] = (end - start) * 1000 #change to ms 
+        
+        redis_rst['success'] = 1
+        redis_rst['data'] = info
+    except:
+        redis_rst['success'] = 0
+        redis_rst['data'] = 'error'
+    
+    return redis_rst
 
 
 @singleton
@@ -15,13 +36,11 @@ class RedisMonitor(object):
     '''
     classdocs
     '''
-    def __init__(self, timeout = 1):
+    def __init__(self):
         '''
         Constructor
         '''
-        self.timeout = timeout #数据过期时间
         self.servers = {}
-        self.redis_requesting = {} #全局控制请求，尽量减少请求数
         
     def _md5(self, s):
         '''
@@ -50,55 +69,10 @@ class RedisMonitor(object):
             #如果host，port非法，则直接返回错误
             return {'success': 0, 'data': 'parameter error'}
     
-    def new_request(self, key, host, port, password, charset = 'utf8'):
-        redis_rst = {}
-        #需要重新请求获得数据
-        self.redis_requesting[key] = True
-        try:
-            start = time.time()
-            
-            r = redis.Redis(host = host, port = int(port), password = password)
-            info = r.info()
-            end = time.time();
-            info['get_time'] = (end - start) * 1000 #change to ms 
-            
-            redis_rst['success'] = 1
-            redis_rst['data'] = info
-        except:
-            redis_rst['success'] = 0
-            redis_rst['data'] = 'error'
-        #将获取的redis信息保存到内存中，缓存起来
-        self.servers[key] = {}
-        self.servers[key]['time'] = time.time() #更新时间
-        self.servers[key]['info'] = redis_rst
-        try:
-            #可能出现键值不存在的情况，捕获以下
-            self.redis_requesting.pop(key)
-        except:
-            pass
-        return redis_rst
-    
     def get_info(self, host, port, password, charset = 'utf8'):
         redis_rst = {}
         if host and port:
-            key = self._md5(host + str(port))
-            #小于timeout，不用重新连接redis获取信息，直接拉缓存数据
-            if key in self.servers.keys():
-                if self.redis_requesting.get(key, False):
-                    redis_rst = self.servers[key]['info']
-                else:
-                    if time.time() - self.servers[key]['time'] < self.timeout:
-                        redis_rst = self.servers[key]['info']
-                    else:
-                        #已经过期
-                        redis_rst = self.new_request(key, host, port, password, charset)
-            else:
-                if self.redis_requesting.get(key, False):
-                    redis_rst = {'success': 0, 'data': 'requesting'}
-                else:
-                    #第一次请求数据
-                    redis_rst = self.new_request(key, host, port, password, charset)
-                
+            redis_rst = new_request(host, port, password, charset)
         else:
             #如果host，port非法，则直接返回错误
             redis_rst = {'success': 0, 'data': 'parameter error'}
